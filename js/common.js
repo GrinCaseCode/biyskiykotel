2a9407f186f24ff0d4430df8d59acd279d208ff3
nickolay.grin <nick@singularitysolutions.com>
2025-10-17 12:09:48 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,332 @@
+$(document).ready(function () {
+
+
+	//прилипающие меню
+	var $menu = $(".header");
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 0 && $menu.hasClass("default")) {
+			$menu.removeClass("default").addClass("fixed");
+		} else if ($(this).scrollTop() <= 0 && $menu.hasClass("fixed")) {
+			$menu.removeClass("fixed").addClass("default");
+		}
+
+	});
+
+	if ($(this).scrollTop() > 0 && $menu.hasClass("default")) {
+		$menu.removeClass("default").addClass("fixed");
+	} else if ($(this).scrollTop() <= 0 && $menu.hasClass("fixed")) {
+		$menu.removeClass("fixed").addClass("default");
+	}
+
+
+	/*input file*/
+	$("input[type='file']").change(function () {
+		var filename_text = $(this).parent().siblings(".name-upload");
+		var filename = $(this).val().replace(/.*\\/, "");
+		filename_text.html(filename);
+	});
+
+
+	//кнопка sandwich
+	$(".sandwich").click(function () {
+		if (!$(".header__content").hasClass("active")) {
+			$(".header__content").addClass("active");
+			$(".sandwich").addClass("active");
+			$("body").addClass("no-scroll");
+			$(".menu-overlay").fadeIn(200);
+		} else {
+			$(".header__content").removeClass("active");
+			$(".sandwich").removeClass("active");
+			$("body").removeClass("no-scroll");
+			$(".menu-overlay").fadeOut(200);
+		}
+	});
+
+	$(".menu-overlay").click(function () {
+		$(".header__content").removeClass("active");
+		$(".sandwich").removeClass("active");
+		$("body").removeClass("no-scroll");
+		$(".menu-overlay").fadeOut(200);
+	});
+
+
+	$(".btn-catalog").click(function (e) {
+		e.preventDefault();
+		if ($(".catalog-dropdown").is(":hidden")) {
+			$(".catalog-dropdown").slideDown(200);
+			$(this).addClass("active");
+		} else {
+			$(".catalog-dropdown").slideUp(200);
+			$(this).removeClass("active");
+		}
+	});
+
+	{
+		if ($(window).width() > 992) {
+			$(document).mouseup(function (e) {
+				var container = $(".catalog-wrap");
+				if (container.has(e.target).length === 0) {
+					$(".catalog-dropdown").slideUp(200);
+					$(".btn-catalog").removeClass("active");
+				}
+			});
+
+			$(".catalog-dropdown__haschild").on("mouseenter", function () {
+				$(this).siblings().removeClass("active");
+				$(this).addClass("active");
+			});
+
+			$(".close-dropdown").on("click", function () {
+				$(this).parent().removeClass("active");
+			});
+		}
+	}
+
+	{
+		if ($(window).width() < 992) {
+			$(".close-dropdown").click(function () {
+				$(this).parent().siblings().find("ul").slideUp(200);
+				$(this).parent().siblings().find(".close-dropdown").removeClass("active");
+				if ($(this).siblings("ul").is(":hidden")) {
+					$(this).siblings("ul").slideDown(200);
+					$(this).addClass("active");
+				} else {
+					$(this).siblings("ul").slideUp(200);
+					$(this).removeClass("active");
+				}
+			});
+
+			$(".menu i").click(function () {
+				$(this).parent().siblings().find("ul").slideUp(200);
+				$(this).parent().siblings().find("i").removeClass("active");
+				if ($(this).siblings("ul").is(":hidden")) {
+					$(this).siblings("ul").slideDown(200);
+					$(this).addClass("active");
+				} else {
+					$(this).siblings("ul").slideUp(200);
+					$(this).removeClass("active");
+				}
+			});
+
+			$(".btn-wrap .btn-main").click(function (e) {
+				e.preventDefault();
+				$(".btn-popup").slideToggle(200);
+			});
+
+			$(document).mouseup(function (e) {
+				var container2 = $(".btn-wrap");
+				if (container2.has(e.target).length === 0) {
+					$(".btn-popup").slideUp(200);
+				}
+			});
+
+		}
+	}
+
+
+
+
+	//questions
+	$(".item-question__head").click(function () {
+		$(this).parent().toggleClass("active");
+		$(this).siblings().slideToggle(200);
+		$(this).parent().siblings(".item-question").removeClass("active");
+		$(this).parent().siblings(".item-question").find(".item-question__content").slideUp(200);
+	});
+
+	//слайдер
+
+	$('.slider-billbord').slick({
+		arrows: false,
+		dots: false,
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		touchThreshold: 1000,
+		speed: 1000,
+		fade: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: '<div class="slick-prev slick-arrow"><i class="far fa-long-arrow-left"></i><div/>',
+		nextArrow: '<div class="slick-next slick-arrow"><i class="far fa-long-arrow-right"></i><div/>',
+	});
+
+	$('.slider-documents').slick({
+		arrows: true,
+		dots: false,
+		infinite: true,
+		touchThreshold: 1000,
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		prevArrow: '<div class="slick-prev slick-arrow"><i class="far fa-long-arrow-left"></i><div/>',
+		nextArrow: '<div class="slick-next slick-arrow"><i class="far fa-long-arrow-right"></i><div/>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 4,
+					arrows: false,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					arrows: false,
+					dots: true
+				}
+			}
+		]
+	});
+
+	$('.slider-reviews').slick({
+		arrows: true,
+		dots: false,
+		infinite: true,
+		touchThreshold: 1000,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		prevArrow: '<div class="slick-prev slick-arrow"><i class="far fa-long-arrow-left"></i><div/>',
+		nextArrow: '<div class="slick-next slick-arrow"><i class="far fa-long-arrow-right"></i><div/>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2,
+					arrows: false,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					arrows: false,
+					dots: true
+				}
+			}
+		]
+	});
+
+	var rev = $('.slider-advantages');
+	rev.on('init', function (event, slick, currentSlide) {
+		var
+			cur = $(slick.$slides[slick.currentSlide]),
+			next = cur.next(),
+			next2 = cur.next().next(),
+			prev = cur.prev(),
+			prev2 = cur.prev().prev();
+		prev.addClass('slick-sprev');
+		next.addClass('slick-snext');
+		prev2.addClass('slick-sprev2');
+		next2.addClass('slick-snext2');
+		cur.removeClass('slick-snext').removeClass('slick-sprev').removeClass('slick-snext2').removeClass('slick-sprev2');
+		slick.$prev = prev;
+		slick.$next = next;
+	}).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+		console.log('beforeChange');
+		var
+			cur = $(slick.$slides[nextSlide]);
+		console.log(slick.$prev, slick.$next);
+		slick.$prev.removeClass('slick-sprev');
+		slick.$next.removeClass('slick-snext');
+		slick.$prev.prev().removeClass('slick-sprev2');
+		slick.$next.next().removeClass('slick-snext2');
+		next = cur.next(),
+			prev = cur.prev();
+		prev.addClass('slick-sprev');
+		next.addClass('slick-snext');
+		prev.prev().addClass('slick-sprev2');
+		next.next().addClass('slick-snext2');
+		slick.$prev = prev;
+		slick.$next = next;
+		cur.removeClass('slick-next').removeClass('slick-sprev').removeClass('slick-next2').removeClass('slick-sprev2');
+	});
+
+	rev.slick({
+		speed: 1000,
+		arrows: true,
+		dots: false,
+		focusOnSelect: true,
+		infinite: true,
+		centerMode: true,
+		appendArrows: $(".controls-slider"),
+		slidesPerRow: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		centerPadding: '0',
+		swipe: true,
+		customPaging: function (slider, i) {
+			return '';
+		},
+	});
+
+	$('.slider-card').slick({
+		arrows: true,
+		dots: true,
+		infinite: true,
+		touchThreshold: 1000,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: '<div class="slick-prev slick-arrow"><i class="far fa-long-arrow-left"></i><div/>',
+		nextArrow: '<div class="slick-next slick-arrow"><i class="far fa-long-arrow-right"></i><div/>',
+	});
+
+		$('.slider-images').slick({
+		arrows: true,
+		dots: false,
+		infinite: true,
+		touchThreshold: 1000,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: '<div class="slick-prev slick-arrow"><i class="far fa-long-arrow-left"></i><div/>',
+		nextArrow: '<div class="slick-next slick-arrow"><i class="far fa-long-arrow-right"></i><div/>',
+	});
+
+	
+  $('.tabs li a').click(function(event) {
+    event.preventDefault();
+    $(this).parent().parent().find("li").removeClass('active');
+    $(this).parent().addClass('active');
+    $(".tab-pane").fadeOut(0);
+    var selectTab = $(this).attr("href");
+    $(selectTab).fadeIn(200);
+	$('.tab-pane').find(".slider-card").slick('setPosition');
+	$('.tab-pane').find(".slider-images").slick('setPosition');
+  });
+
+	$(".input-phone").mask("+7 (999) 999-99-99");
+
+
+	// стайлер для select
+	$('select').styler();
+
+	//Попап менеджер FancyBox
+	$(".fancybox").fancybox({
+		autoFocus: false,
+		backFocus: false,
+	});
+
+
+	//Кнопка "Наверх"
+	$(".btn_top").click(function () {
+		$("body, html").animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+
+	objectFitImages();
+
+
+});
+
+
+/*polifyl*/
+/*! npm.im/object-fit-images 3.2.4 */
+var objectFitImages = function () { "use strict"; function t(t, e) { return "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='" + t + "' height='" + e + "'%3E%3C/svg%3E" } function e(t) { if (t.srcset && !p && window.picturefill) { var e = window.picturefill._; t[e.ns] && t[e.ns].evaled || e.fillImg(t, { reselect: !0 }), t[e.ns].curSrc || (t[e.ns].supported = !1, e.fillImg(t, { reselect: !0 })), t.currentSrc = t[e.ns].curSrc || t.src } } function i(t) { for (var e, i = getComputedStyle(t).fontFamily, r = {}; null !== (e = u.exec(i));)r[e[1]] = e[2]; return r } function r(e, i, r) { var n = t(i || 1, r || 0); b.call(e, "src") !== n && h.call(e, "src", n) } function n(t, e) { t.naturalWidth ? e(t) : setTimeout(n, 100, t, e) } function c(t) { var c = i(t), o = t[l]; if (c["object-fit"] = c["object-fit"] || "fill", !o.img) { if ("fill" === c["object-fit"]) return; if (!o.skipTest && f && !c["object-position"]) return } if (!o.img) { o.img = new Image(t.width, t.height), o.img.srcset = b.call(t, "data-ofi-srcset") || t.srcset, o.img.src = b.call(t, "data-ofi-src") || t.src, h.call(t, "data-ofi-src", t.src), t.srcset && h.call(t, "data-ofi-srcset", t.srcset), r(t, t.naturalWidth || t.width, t.naturalHeight || t.height), t.srcset && (t.srcset = ""); try { s(t) } catch (t) { window.console && console.warn("https://bit.ly/ofi-old-browser") } } e(o.img), t.style.backgroundImage = 'url("' + (o.img.currentSrc || o.img.src).replace(/"/g, '\\"') + '")', t.style.backgroundPosition = c["object-position"] || "center", t.style.backgroundRepeat = "no-repeat", t.style.backgroundOrigin = "content-box", /scale-down/.test(c["object-fit"]) ? n(o.img, function () { o.img.naturalWidth > t.width || o.img.naturalHeight > t.height ? t.style.backgroundSize = "contain" : t.style.backgroundSize = "auto" }) : t.style.backgroundSize = c["object-fit"].replace("none", "auto").replace("fill", "100% 100%"), n(o.img, function (e) { r(t, e.naturalWidth, e.naturalHeight) }) } function s(t) { var e = { get: function (e) { return t[l].img[e ? e : "src"] }, set: function (e, i) { return t[l].img[i ? i : "src"] = e, h.call(t, "data-ofi-" + i, e), c(t), e } }; Object.defineProperty(t, "src", e), Object.defineProperty(t, "currentSrc", { get: function () { return e.get("currentSrc") } }), Object.defineProperty(t, "srcset", { get: function () { return e.get("srcset") }, set: function (t) { return e.set(t, "srcset") } }) } function o() { function t(t, e) { return t[l] && t[l].img && ("src" === e || "srcset" === e) ? t[l].img : t } d || (HTMLImageElement.prototype.getAttribute = function (e) { return b.call(t(this, e), e) }, HTMLImageElement.prototype.setAttribute = function (e, i) { return h.call(t(this, e), e, String(i)) }) } function a(t, e) { var i = !y && !t; if (e = e || {}, t = t || "img", d && !e.skipTest || !m) return !1; "img" === t ? t = document.getElementsByTagName("img") : "string" == typeof t ? t = document.querySelectorAll(t) : "length" in t || (t = [t]); for (var r = 0; r < t.length; r++)t[r][l] = t[r][l] || { skipTest: e.skipTest }, c(t[r]); i && (document.body.addEventListener("load", function (t) { "IMG" === t.target.tagName && a(t.target, { skipTest: e.skipTest }) }, !0), y = !0, t = "img"), e.watchMQ && window.addEventListener("resize", a.bind(null, t, { skipTest: e.skipTest })) } var l = "fregante:object-fit-images", u = /(object-fit|object-position)\s*:\s*([-.\w\s%]+)/g, g = "undefined" == typeof Image ? { style: { "object-position": 1 } } : new Image, f = "object-fit" in g.style, d = "object-position" in g.style, m = "background-size" in g.style, p = "string" == typeof g.currentSrc, b = g.getAttribute, h = g.setAttribute, y = !1; return a.supportsObjectFit = f, a.supportsObjectPosition = d, o(), a }();
+
